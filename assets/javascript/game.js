8ec58dceb0f39f240e0fb2ff9dbed5c4f8335800
mwdeal1 <mwdeal1@gmail.com>
2016-12-12 19:43:18 -0500

    Add todo items

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+// character class
+// healthPoints, baseAttackPower, attackPower, counterAttackPower
+
+// game class
+// GAME_STATES - Character Select, Defender Select, In Battle, Victory, Game Over
+// initialize() - create characters, set game state
+// resolveAttack(character1, character2)
+// player (character)
+// defenders[] (character)
+
